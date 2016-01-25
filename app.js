620225d214e3e359af244282b624957e9f47b9fe
Damian <damian@sta.marketing>
2016-01-25 10:21:49 -0300

    Creados los archivos base

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var router = require('./routes');
+
+app.use(function(req, res, next) {
+	var oneof = false;	
+    if(req.headers.origin) {
+        res.header('Access-Control-Allow-Origin', req.headers.origin);
+        oneof = true;
+    }
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Authorization, Content-Type, Accept, Origin");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    if(oneof) {
+        res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+    }
+
+    if (req.method == 'OPTIONS')
+        res.status(200).send();
+    else
+        next();
+});
+
+
+//Set bodyParser middleware
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+//Set routers
+app.use('/', router);
+
+app.listen(3000, function () {
+  console.log('Puerto '+3000);
+});
